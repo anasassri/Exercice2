ddaa11305b2e10d50cd1c49ab4abb298f5e58d5e
Anas El Assri <102320886+anasowen@users.noreply.github.com>
2022-10-30 22:03:34 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    
+    public static void Main()
+    {
+        int N1;
+        N1 = 3;
+        int[] MyNum = new int[7] ;
+        MyNum[0] = 1;
+        MyNum[1] = 2;
+        MyNum[2] = 3;
+        MyNum[3] = 4;
+        MyNum[4] = 5;
+        MyNum[5]= 6;
+        MyNum[6] = N1;
+        int tmp;
+        for(int i=MyNum.Length-2 ,j= MyNum.Length-1 ; MyNum[i]> MyNum[j]; i--,j--){
+            tmp = MyNum[i];
+            MyNum[i] = MyNum[j];
+            MyNum[j] = tmp;
+
+        }
+
+        for (int i = 0; i < MyNum.Length; i++)
+        {
+            Console.WriteLine(MyNum[i]);
+        }
+
+
+    }
+}
